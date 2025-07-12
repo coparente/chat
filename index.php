caaafa91ce246b63b41aff228cce4d4aee9c6967
unknown <Tjgo@SECEXECJUD16.tjgo.gov>
2025-07-12 18:34:53 -0300

    Iniciando o projeto 05

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 include 'vendor/autoload.php';
 include 'config/app.php';
@@ -10,18 +11,10 @@ $db = new Database;
 // Carregar sistema de rotas
 require_once 'app/Libraries/Route.php';
 
-// Incluir head apenas se não for uma requisição AJAX
-// if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
-//     include 'app/Views/include/head.php';
-// }
-
 // Carregar rotas definidas
 require_once 'routes/web.php';
 
 // Processar rotas
 Route::dispatch();
 
-// Incluir scripts apenas se não for uma requisição AJAX
-// if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
-    // include 'app/Views/include/linkjs.php';
-// }
+ob_end_flush();
